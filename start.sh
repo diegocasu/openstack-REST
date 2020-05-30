825af64eba25cf5b81f8f4f505cf8ec994130c59
francescobarbarulo <f.barbarulo@studenti.unipi.it>
2020-05-30 17:27:46 +0100

    Added python-openstackclient in requirements.txt for creating flavors at platform initializaiton. Updated start.sh for creating standard and large flavors. Implemented servers post.

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Activate the authentication file
 source admin-openrc.sh
+
+# Start cron deamon for running scheduled tasks
 crond
+
+# Create two flavors for requirements:
+# 1) Standard
+openstack flavor create --public standard --id auto --ram 128 --disk 1 --vcpus 2
+# 2) Large
+openstack flavor create --public large --id auto --ram 256 --disk 1 --vcpus 4
+
+# Start the REST server
 python3 -m swagger_server
--- swagger_server/controllers/servers_controller.py
+++ swagger_server/controllers/servers_controller.py
@@ -31,4 +31,32 @@ def servers_post(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Server.from_dict(connexion.request.get_json())  # noqa: E501
-    return body
+        
+        conn = openstack.connect()
+
+        if body.name is None or body.image is None or body.flavor is None or body.network is None:
+            return 'name, image, flavor, network are required', 400
+
+        name = body.name
+        image = conn.compute.find_image(body.image)
+        flavor = conn.compute.find_flavor(body.flavor)
+        network = conn.network.find_network(body.network)
+
+        if image is None:
+            return 'Cannot find image {}'.format(body.image), 400
+
+        if flavor is None:
+            return 'Cannot find flavor {}'.format(body.flavor), 400
+
+        if network is None:
+            return 'Cannot find network {}'.format(body.network), 400
+
+        print('Create server with:\nname: {}\nimage: {}\nflavor: {}\nnetwork: {}'.format(name, image.id, flavor.id, network.id))
+    
+        server = conn.compute.create_server(
+                name=name, image_id=image.id, flavor_id=flavor.id,
+                networks=[{"uuid": network.id}])
+
+        server = conn.compute.wait_for_server(server)
+    
+    return server.id
