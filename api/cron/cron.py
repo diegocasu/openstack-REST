cc6347533a46507be55f69fe646f47258d9b3524
Giovan Battista Rolandi <g.rolandi@studenti.unipi.it>
2020-06-25 14:00:51 +0000

    minor changes for log format

--- api/cron/cron.py
+++ api/cron/cron.py
@@ -15,10 +15,10 @@ except FileNotFoundError:
 conn = openstack.connect()
 
 for index, schedule in enumerate(database):
-    print('[{}] probing {} ({}-{})\t'.format(current_time_string, schedule['id'], schedule['peak_interval']['start'], schedule['peak_interval']['end']), end='')
+    print('[{}, {}] probing {} ({}-{})\t'.format(current_time_string, index, schedule['id'], schedule['peak_interval']['start'], schedule['peak_interval']['end']), end='')
 
     if schedule['peak_interval']['start'] == current_time_string:
-        print('starting vm')
+        print('starting vms')
         IMAGE_NAME = schedule['server']['image']
         FLAVOR_NAME = schedule['server']['flavor']
         NETWORK_NAME = schedule['server']['network']
@@ -40,7 +40,7 @@ for index, schedule in enumerate(database):
         schedule['server']['ids'] = ids
         database[index] = schedule
 
-        print(database)
+        # print(database)
 
         try:
             pickle.dump(database, open(SCHEDULE_DATABASE, 'wb'))
@@ -49,7 +49,7 @@ for index, schedule in enumerate(database):
 
 
     elif schedule['peak_interval']['end'] == current_time_string:
-        print('stopping vm ', end='')
+        print('stopping vms')
         for id in schedule['server']['ids']:
             server = conn.compute.find_server(id)
             conn.compute.delete_server(server)
